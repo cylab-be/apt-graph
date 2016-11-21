4e343c95edd45ead98bd1ba088fb3a198f2689d8
Thibault Debatty <thibault.debatty@gmail.com>
2016-11-21 12:37:11 +0100

    Integration test between batch processor and json-rpc server. Fixes issue #11

---
+++ integration/src/test/java/aptgraph/integration/BatchServerIT.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Thibault Debatty.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package aptgraph.integration;
+
+import aptgraph.batch.BatchProcessor;
+import aptgraph.core.Request;
+import aptgraph.server.JsonRpcServer;
+import aptgraph.server.RequestHandler;
+import info.debatty.java.graphs.Graph;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.TestCase;
+
+/**
+ * Integration test for the batch processor + JSON RPC Server.
+ * Create a graph with batch processor and check it is correctly used by server.
+ *
+ * @author Thibault Debatty
+ */
+public class BatchServerIT extends TestCase {
+
+    private static final int STARTUP_DELAY = 5000;
+    private volatile Exception server_thread_ex = null;
+
+    public void testSomeMethod() throws IOException, ClassNotFoundException, InterruptedException, Exception {
+        System.out.println("Integration test for batch processor + JSON RPC Server");
+        System.out.println("Test batch server with 1000 reqs");
+        File temp_file = File.createTempFile("tempfile", ".tmp");
+
+        BatchProcessor processor = new BatchProcessor();
+        processor.analyze(
+                getClass().getResourceAsStream("/1000_http_requests.txt"),
+                new FileOutputStream(temp_file));
+
+
+        final JsonRpcServer server = new JsonRpcServer(
+                new FileInputStream(temp_file));
+
+        // Start the server in a separate thread, so we can wait and stop it
+        // from the main thread...
+        Thread server_thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    server.start();
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                    server_thread_ex = ex;
+
+                } catch (Exception ex) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                    server_thread_ex = ex;
+                }
+            }
+        });
+
+        server_thread.start();
+
+        // wait a little...
+        Thread.sleep(STARTUP_DELAY);
+
+        // Ask the server to stop
+        server.stop();
+
+        // Wait for the server to finish...
+        server_thread.join();
+
+        // Throw the exception in the main thread, to mark the test as failed
+        if (server_thread_ex != null) {
+            throw server_thread_ex;
+        }
+    }
+
+}
--- server/src/test/java/aptgraph/server/RequestHandlerTest.java
+++ server/src/test/java/aptgraph/server/RequestHandlerTest.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.util.LinkedList;
-import java.util.List;
 import junit.framework.TestCase;
 
 /**
