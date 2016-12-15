b0c08a31d720a569928142f1c31dfaebda8d1553
Thibault Debatty <thibault.debatty@gmail.com>
2016-12-15 13:50:21 +0100

    Added grunt eslint tests

--- website/Gruntfile.js
+++ website/Gruntfile.js
@@ -2,12 +2,17 @@ module.exports = function(grunt) {
  
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        
         jshint: {
             all: ['Gruntfile.js', 'js/graph.js']
+        },
+        eslint: {
+            src: ["js/graph.js"]
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-jshint');
- 
-    grunt.registerTask('default', ['jshint']);
- 
+    grunt.loadNpmTasks("gruntify-eslint");
+
+    grunt.registerTask('default', ['eslint', 'jshint']); 
 };
