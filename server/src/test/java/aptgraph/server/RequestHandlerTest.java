c99121650494fc15bced9f44236bc57db196d4d7
tgilon <thomas.gilon+github@gmail.com>
2017-02-16 07:42:19 +0100

    Minor bug fix

--- server/src/test/java/aptgraph/server/RequestHandlerTest.java
+++ server/src/test/java/aptgraph/server/RequestHandlerTest.java
@@ -90,6 +90,7 @@ public class RequestHandlerTest extends TestCase {
         input.close();
 
         RequestHandler handler = new RequestHandler(user_graphs);
-        handler.analyze("127.0.0.1", new double[]{1.0, 1.0}, new double[]{1.0, 0.2}, 10.0, 10);
+        handler.analyze("127.0.0.1", new double[]{0.7, 0.3},
+                new double[]{0.8, 0.2}, 10.0, 10);
     }
 }
