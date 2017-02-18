9122f830de800321231952b41ec8fd39b2666469
tgilon <thomas.gilon+github@gmail.com>
2017-02-18 08:53:41 +0100

    Conceptual Correction : temporal children for all features

--- server/src/main/java/aptgraph/server/RequestHandler.java
+++ server/src/main/java/aptgraph/server/RequestHandler.java
@@ -127,13 +127,13 @@ public class RequestHandler {
             return null;
         }
 
-        // Selection of the temporal children only
-        graphs = childrenSelection(graphs);
-
         // Fusion of the features (Graph of Requests)
         Graph<Request> merged_graph =
                 computeFusionFeatures(graphs,
                         feature_ordered_weights, feature_weights);
+
+        // Selection of the temporal children only
+        merged_graph = childrenSelection(merged_graph);
 
         // The json-rpc request was probably canceled by the user
         if (Thread.currentThread().isInterrupted()) {
@@ -321,30 +321,22 @@ public class RequestHandler {
 
     /**
      * Select only the temporal children.
-     * @param graphs
-     * @return graphs
+     * @param graph
+     * @return graph
      */
-    private LinkedList<Graph<Request>> childrenSelection(
-            final LinkedList<Graph<Request>> graphs) {
-        Graph<Request> time_graph = graphs.getFirst();
-        Graph<Request> time_graph_new = new Graph<Request>();
-        for (Request req : time_graph.getNodes()) {
+    private Graph<Request> childrenSelection(
+            final Graph<Request> graph) {
+        Graph<Request> graph_new = new Graph<Request>();
+        for (Request req : graph.getNodes()) {
             NeighborList neighbors_new = new NeighborList(1000);
-            NeighborList neighbors = time_graph.getNeighbors(req);
+            NeighborList neighbors = graph.getNeighbors(req);
             for (Neighbor<Request> neighbor : neighbors) {
                 if (req.getTime() < neighbor.node.getTime()) {
                     neighbors_new.add(neighbor);
                 }
             }
-            time_graph_new.put(req, neighbors_new);
+            graph_new.put(req, neighbors_new);
         }
-        LinkedList<Graph<Request>> graphs_new =
-                new LinkedList<Graph<Request>>();
-        graphs_new.add(time_graph_new);
-        // Add up all others features
-        for (int i = 1; i < graphs.size(); i++) {
-            graphs_new.add(graphs.get(i));
-        }
-        return graphs_new;
+        return graph_new;
     }
 }
