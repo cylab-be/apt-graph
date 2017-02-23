234b117fe377cd13b897be976881b5e8fe625e56
tgilon <thomas.gilon+github@gmail.com>
2017-02-23 22:51:57 +0100

    Change of the definition of DomainSimilarity
    Does not take into account the Top Level Domain for similarity.

--- core/src/main/java/aptgraph/core/DomainSimilarity.java
+++ core/src/main/java/aptgraph/core/DomainSimilarity.java
@@ -20,7 +20,7 @@ public class DomainSimilarity
         double counter = 0.0;
         String[] domain_r1 = r1.getDomain().split("[.]");
         String[] domain_r2 = r2.getDomain().split("[.]");
-        for (int i = 0; i <= Math.min(domain_r1.length,
+        for (int i = 1; i <= Math.min(domain_r1.length,
                 domain_r2.length) - 1; i++) {
             if (domain_r1[domain_r1.length - i - 1]
                     .equals(domain_r2[domain_r2.length - i - 1])) {
@@ -29,6 +29,6 @@ public class DomainSimilarity
                 break;
             }
         }
-        return counter / Math.max(domain_r1.length, domain_r2.length);
+        return counter / (Math.max(domain_r1.length, domain_r2.length) - 1);
     }
 }
