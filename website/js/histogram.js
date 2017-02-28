454e7698d2d180f77a5504beaa4d92c748864888
tgilon <thomas.gilon+github@gmail.com>
2017-02-28 12:03:43 +0100

    UI with histogram

---
+++ website/js/histogram.js
@@ -0,0 +1,51 @@
+// jscs:disable
+function draw_hist(position) {
+    var width = 900;
+    var height = 300;
+    var pad = 20;
+    var leftPad = 100;
+
+    var x = d3.scale.ordinal().rangeRoundBands([leftPad, width - pad], 0.1);
+    var y = d3.scale.linear().range([height - pad, pad]);
+
+    var xAxis = d3.svg.axis().scale(x).orient('bottom');
+    var yAxis = d3.svg.axis().scale(y).orient('left');
+
+    var svg = d3.select(position)
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height);
+
+    d3.json('histogram_hours.json', function(data) {
+        data = d3.keys(data).map(function(key) {
+            return {bucket: Number(key), N: data[key]};
+          });
+
+        x.domain(data.map(function(d) { return d.bucket; }));
+        y.domain([0, d3.max(data, function(d) { return d.N; })]);
+
+        svg.append('g')
+            .attr('class', 'axis')
+            .attr('transform', 'translate(0, ' + (height - pad) + ')')
+            .call(xAxis);
+
+        svg.append('g')
+            .attr('class', 'axis')
+            .attr('transform', 'translate(' + (leftPad - pad) + ', 0)')
+            .call(yAxis);
+
+        svg.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('class', 'bar')
+            .attr('x', function(d) { return x(d.bucket); })
+            .attr('width', x.rangeBand())
+            .attr('y', height - pad)
+            .transition()
+            .delay(function(d) { return d.bucket * 20; })
+            .duration(800)
+            .attr('y', function(d) { return y(d.N); })
+            .attr('height', function(d) { return height - pad  -  y(d.N); });
+      });
+}
