73d154780877605ab6533b4bcd837801fe31df65
tgilon <thomas.gilon+github@gmail.com>
2017-03-04 20:24:20 +0100

    Minor print fix

--- server/src/main/java/aptgraph/server/JsonRpcServer.java
+++ server/src/main/java/aptgraph/server/JsonRpcServer.java
@@ -98,7 +98,7 @@ public class JsonRpcServer {
         LOGGER.log(Level.INFO, "k-NN Graph : k = {0}",
                 user_graphs.get(first_key).getFirst().getK());
         LOGGER.log(Level.INFO, "Starting JSON-RPC server at http://{0}:{1}",
-                new Object[]{config.getServerHost(), config.getServerPort()});
+             new Object[]{config.getServerHost(), "" + config.getServerPort()});
 
         RequestHandler request_handler =
                 new RequestHandler(user_graphs);
