687f7a630a266d61f18d1a86893bc04bd8a9161e
tgilon <thomas.gilon+github@gmail.com>
2017-04-12 15:25:28 +0200

    Domain Similarity : bug fix
    The two domains need to have the same TLD.

--- core/src/main/java/aptgraph/core/DomainSimilarity.java
+++ core/src/main/java/aptgraph/core/DomainSimilarity.java
@@ -20,7 +20,9 @@ public class DomainSimilarity
         double counter = 0.0;
         String[] domain_r1 = r1.getDomain().split("[.]");
         String[] domain_r2 = r2.getDomain().split("[.]");
-        if (domain_r1.length > 1 && domain_r2.length > 1) {
+        if (domain_r1[domain_r1.length - 1]
+                .equals(domain_r2[domain_r2.length - 1])
+                && domain_r1.length > 1 && domain_r2.length > 1) {
             for (int i = 1; i <= Math.min(domain_r1.length,
                     domain_r2.length) - 1; i++) {
                 if (domain_r1[domain_r1.length - i - 1]
